2e81479469ff9020d919420499b8e37e64bc1b09
Gabriel-Vignoli <pessoalvignoli123@gmail.com>
2024-11-08 21:34:47 -0300

    chore: add shadcn setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import { Button } from "./_components/ui/button";
+
 export default function Home() {
-  return <h1 className="p-5 text-red-500">Hello World</h1>;
+  return <Button>Hello Gabriel</Button>;
 }
