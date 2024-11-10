02303d88f9fa6f08b2a62480da196b5c52cb3ae2
Gabriel-Vignoli <pessoalvignoli123@gmail.com>
2024-11-09 21:56:03 -0300

    feat: add authentication with clerk

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,22 +1,37 @@
 import Image from "next/image";
 import { Button } from "../_components/ui/button";
 import { LogInIcon } from "lucide-react";
+import { SignInButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-const LoginPage = () => {
+const LoginPage = async () => {
+  const { userId } = await auth();
+  if (userId) {
+    redirect("/");
+  }
   return (
     <div className="grid h-full grid-cols-2">
-      <div className="flex h-full flex-col justify-center p-8 max-w-[550px] mx-auto">
-        <Image src="/soon.svg" alt="Finance AI" width={173} height={39} className="mb-8"/>
-        <h1 className="text-4xl font-bold mb-3">Welcome</h1>
+      <div className="mx-auto flex h-full max-w-[550px] flex-col justify-center p-8">
+        <Image
+          src="/soon.svg"
+          alt="Finance AI"
+          width={173}
+          height={39}
+          className="mb-8"
+        />
+        <h1 className="mb-3 text-4xl font-bold">Welcome</h1>
         <p className="mb-8">
           Finance AI is a financial management platform that uses AI to monitor
           your transactions and provide personalized insights, making it easier
           to manage your budget.
         </p>
-        <Button variant="outline">
-            <LogInIcon className="mr-2"/>
+        <SignInButton>
+          <Button>
+            <LogInIcon className="mr-2" />
             Login or create an account
-        </Button>
+          </Button>
+        </SignInButton>
       </div>
       <div className="relative h-full w-full">
         <Image src="/login.png" alt="Login" fill className="object-cover" />
