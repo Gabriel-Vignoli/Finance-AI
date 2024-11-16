ef6b5d4d0c1d7198b4c2d6eb76f044193105131d
Gabriel-Vignoli <pessoalvignoli123@gmail.com>
2024-11-16 17:39:29 -0300

    feat: add navbar component

---
+++ app/_components/navbar.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const Navbar = () => {
+  const pathname = usePathname();
+  return (
+    <nav className="flex justify-between border-b border-solid px-8 py-4">
+      <div className="flex items-center gap-10">
+        <Image src="/soon.svg" width={173} height={39} alt="Finace AI"></Image>
+        <Link
+          href="/"
+          className={
+            pathname === "/"
+              ? "font-bold text-primary"
+              : "text-muted-foreground"
+          }
+        >
+          Dashboard
+        </Link>
+        <Link
+          href="/transactions"
+          className={
+            pathname === "/transactions"
+              ? "font-bold text-primary"
+              : "text-muted-foreground"
+          }
+        >
+          Transactions
+        </Link>
+        <Link
+          href="/subscription"
+          className={
+            pathname === "/subscription"
+              ? "font-bold text-primary"
+              : "text-muted-foreground"
+          }
+        >
+          Subscription
+        </Link>
+      </div>
+      <UserButton showName></UserButton>
+    </nav>
+  );
+};
+
+export default Navbar;
