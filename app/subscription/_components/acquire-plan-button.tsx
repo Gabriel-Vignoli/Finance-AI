a307006f5c8c30b337cd359e9e87115fac6c82f1
Gabriel-Vignoli <pessoalvignoli123@gmail.com>
2024-11-24 17:27:56 -0300

    feat: add stripe webhook that handles subscriptions

--- app/subscription/_components/acquire-plan-button.tsx
+++ app/subscription/_components/acquire-plan-button.tsx
@@ -3,8 +3,11 @@
 import { Button } from "@/app/_components/ui/button";
 import { createStripeCheckout } from "../_actions/create-stripe-checkout";
 import { loadStripe } from "@stripe/stripe-js";
+import { useUser } from "@clerk/nextjs";
+import Link from "next/link";
 
 const AcquirePlanButton = () => {
+  const { user } = useUser();
   const handleAcquirePlanClick = async () => {
     const { sessionId } = await createStripeCheckout();
     if (!process.env.NEXT_PUBLIC_STRIPE_PUBLISHED_KEY) {
@@ -19,10 +22,23 @@ const AcquirePlanButton = () => {
     await stripe.redirectToCheckout({ sessionId });
   };
 
+  const hasPremiunPlan = user?.publicMetadata.subscriptionPlan === "premiun";
+  if (hasPremiunPlan) {
+    return (
+      <Button className="w-full rounded-full font-bold" variant="link">
+        <Link
+          href={`${process.env.NEXT_PUBLIC_STRIPE_CUSTOMER_PORTAL_URL as string}?prefilled_email=${user.emailAddresses[0].emailAddress}`}
+        >
+          Manage plan
+        </Link>
+      </Button>
+    );
+  }
   return (
     <Button
       className="w-full rounded-full font-bold"
       onClick={handleAcquirePlanClick}
+      variant="default"
     >
       Purchase plan
     </Button>
